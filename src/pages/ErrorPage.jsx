83b193f5bb6221009ab50dfbafc232a605a7b2ed
itsjahid01 <jahidnoman0@gmail.com>
2023-05-20 01:46:54 +0600

    navbar done

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,15 +1,16 @@
 import { Link, useRouteError } from "react-router-dom";
-import errorImg from "../assets/error.png";
+import errorImg from "../assets/error.jpg";
 
 const ErrorPage = () => {
   const error = useRouteError();
   //   console.log(error.data);
   return (
-    <div className=" w-2/5 h-2/5 mx-auto mt-20 text-center ">
+    <div className=" w-2/5 h-2/5 mx-auto mt-18 text-center ">
       <img className="w-full rounded" src={errorImg} alt="" />
-      <p className="mt-5 text-xl text-red-500">{error.data}</p>
+      <p className="mt-3 text-xl text-red-500">Page Not Pound!!</p>
+      <p className="mt-2 text-xl text-red-500">{error.data}</p>
       <Link to="/">
-        <button className="  btn  mt-5 ">Go to HomePage</button>
+        <button className="  btn  mt-4 ">Go to HomePage</button>
       </Link>
     </div>
   );
