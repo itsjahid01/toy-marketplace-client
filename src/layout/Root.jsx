14ab5df48a0acd2e4965071b4e05c4fd89555dde
itsjahid01 <jahidnoman0@gmail.com>
2023-05-20 04:10:12 +0600

    register page done

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -6,7 +6,7 @@ const Root = () => {
   return (
     <div>
       <Navbar></Navbar>
-      <div className="">
+      <div className="min-h-[calc(100vh-325px)]">
         <Outlet></Outlet>
       </div>
       <Footer></Footer>
