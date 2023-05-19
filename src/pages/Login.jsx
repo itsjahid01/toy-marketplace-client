c989ed6009fa11e58946640f733bafc3838705a1
itsjahid01 <jahidnoman0@gmail.com>
2023-05-20 04:00:33 +0600

    login page done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,61 @@
+import { Link } from "react-router-dom";
+import login from "../assets/login.jpg";
+import { FaGoogle } from "react-icons/fa";
+
 const Login = () => {
   return (
-    <div>
-      <h1>hgfgvh</h1>
+    <div className="container mx-auto">
+      <div className="md:flex justify-center items-center gap-8 p-8 ">
+        <div className="md:w-1/2 p-5">
+          <img src={login} alt="" />
+        </div>
+        <div className=" md:w-1/2 p-5">
+          <div className="card shadow-2xl p-5">
+            <h1 className="text-3xl font-bold text-center">Login</h1>
+            <form className="">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="Email"
+                  name="email"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  className="input input-bordered"
+                />
+              </div>
+              {/* <p className="text-red-500">{error}</p> */}
+              <div className="form-control w-4/6 mx-auto mt-6">
+                <button className="btn">Login</button>
+              </div>
+            </form>
+            <p className="mt-3 mb-3 text-center font-semibold">
+              Don't have an account?
+              <Link to="/register" className="link-primary">
+                Sign Up
+              </Link>
+            </p>
+            <p className="text-center font-semibold">or Login With</p>
+            <button className="btn btn-outline w-4/6 mx-auto mt-3">
+              <span className="mr-2">
+                <FaGoogle />
+              </span>
+              Sign in with Google
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,78 @@
+import { Link } from "react-router-dom";
+import signUp from "../assets/sign-up.png";
+
 const Register = () => {
   return (
-    <div>
-      <h1>gfhngh</h1>
+    <div className="container mx-auto">
+      <div className="md:flex justify-center items-center gap-8 p-8 ">
+        <div className="md:w-1/2 p-5">
+          <img src={signUp} alt="" />
+        </div>
+        <div className=" md:w-1/2 p-5">
+          <div className="card shadow-2xl p-5">
+            <h1 className="text-3xl font-bold text-center">Sign Up</h1>
+            {/* {<p className="text-red-500">{error}</p>} */}
+            <form className="">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Name"
+                  name="name"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="Email"
+                  name="email"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  className="input input-bordered"
+                />
+                {/* {<p className="text-red-500">{passError}</p>} */}
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Photo Url</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Photo Url"
+                  name="photoUrl"
+                  className="input input-bordered"
+                />
+              </div>
+
+              {/* <p className="text-red-500">{error}</p> */}
+              <div className="form-control w-4/6 mx-auto mt-6">
+                <button className="btn">Register</button>
+              </div>
+            </form>
+            <p className="text-center font-semibold mt-3">
+              Already have an account?
+              <Link to="/login" className="link-primary">
+                Log in
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
