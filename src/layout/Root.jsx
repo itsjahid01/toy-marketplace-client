5b0e110e542712badd6a61207e31ecdfe78925f9
itsjahid01 <jahidnoman0@gmail.com>
2023-05-20 17:19:37 +0600

    footer done

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -6,7 +6,7 @@ const Root = () => {
   return (
     <div>
       <Navbar></Navbar>
-      <div className="min-h-[calc(100vh-325px)]">
+      <div className="min-h-[calc(100vh-293px)]">
         <Outlet></Outlet>
       </div>
       <Footer></Footer>
