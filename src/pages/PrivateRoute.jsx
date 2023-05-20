886e1713f1712c0fff14d5a156fdbf7841a95297
itsjahid01 <jahidnoman0@gmail.com>
2023-05-20 21:03:47 +0600

    private route added

--- src/pages/PrivateRoute.jsx
+++ src/pages/PrivateRoute.jsx
@@ -1,9 +1,16 @@
-const PrivateRoute = () => {
-  return (
-    <div>
-      <h2>fdsd</h2>
-    </div>
-  );
+import { useContext } from "react";
+import { AuthContext } from "../AuthProvider/AuthProvider";
+import { Navigate, useLocation } from "react-router-dom";
+
+const PrivateRoute = ({ children }) => {
+  const { user } = useContext(AuthContext);
+  const location = useLocation();
+
+  if (user) {
+    return children;
+  }
+
+  return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
 };
 
 export default PrivateRoute;
